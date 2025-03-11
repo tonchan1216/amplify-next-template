cf092b1657f1e1475ad02fd55e3e83ffa29b14a9
tonchan1216 <cleartone1216@gmail.com>
2025-03-11 08:08:09 +0000

    change

---
+++ actions/postAction.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+
+import { redirect } from "next/navigation";
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export async function postLinkForm(formData: FormData) {
+  const link = formData.get("estate_link") as string;
+  console.log(link)
+
+  const { data: todos } = await client.models.Todo.list()
+  console.log(todos);
+
+  const { data: createResult, errors: createErrors } = await client.models.Todo.create({
+    content: link,
+  });
+  console.log(createErrors)
+  // redirect("/assessment/");
+}
