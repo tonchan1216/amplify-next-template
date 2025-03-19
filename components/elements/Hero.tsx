f4a19b188f6536bf45acb13ee22c68d5b1530f95
tonchan1216 <cleartone1216@gmail.com>
2025-03-19 01:50:08 +0000

    change style

--- components/elements/Hero.tsx
+++ components/elements/Hero.tsx
@@ -7,42 +7,10 @@
 /* eslint-disable */
 import { Button, Flex, Image, Text } from "@aws-amplify/ui-react";
 export default function Hero() {
-  const variants = [
-    {
-      overrides: {
-        Eyebrow: {},
-        Heading: {},
-        Body: {},
-        Message: {},
-        Button: {},
-        HeroMessage: {},
-        Left: {},
-        image: {},
-        Right: {},
-        HeroLayout1: {},
-      },
-      variantValues: { mode: "Light" },
-    },
-    {
-      overrides: {
-        Eyebrow: { color: "rgba(188,236,245,1)", children: "Full-stack" },
-        Heading: { color: "rgba(255,255,255,1)" },
-        Body: { color: "rgba(255,255,255,1)" },
-        Message: {},
-        Button: {},
-        HeroMessage: {},
-        Left: { backgroundColor: "rgba(13,26,38,1)" },
-        Right: {},
-        HeroLayout1: {},
-      },
-      variantValues: { mode: "Dark" },
-    },
-  ];
   return (
     <Flex
       gap="0"
       direction="row"
-      // width="1440px"
       justifyContent="center"
       alignItems="center"
       position="relative"
@@ -86,59 +54,31 @@ export default function Hero() {
             display="flex"
           >
             <Text
-              fontFamily="Inter"
               fontSize="16px"
               fontWeight="700"
               color="rgba(64,170,191,1)"
               lineHeight="24px"
               textAlign="center"
-              display="block"
-              shrink="0"
-              alignSelf="stretch"
-              position="relative"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
               children="Full stack"
             ></Text>
             <Text
-              fontFamily="Inter"
               fontSize="24px"
               fontWeight="600"
               color="rgba(13,26,38,1)"
               lineHeight="30px"
               textAlign="center"
-              display="block"
-              shrink="0"
-              alignSelf="stretch"
-              position="relative"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
               children="Build full-stack web and mobile apps in hours. Easy to start, easy to scale"
             ></Text>
             <Text
-              fontFamily="Inter"
               fontSize="16px"
               fontWeight="400"
               color="rgba(48,64,80,1)"
               lineHeight="24px"
               textAlign="center"
-              display="block"
-              letterSpacing="0.01px"
-              shrink="0"
-              alignSelf="stretch"
-              position="relative"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
               children="AWS Amplify is a complete solution that lets frontend web and mobile developers easily build, ship, and host full-stack applications on AWS, with the flexibility to leverage the breadth of AWS services as use cases evolve. No cloud expertise needed."
             ></Text>
           </Flex>
-          <Button
-            shrink="0"
-            size="large"
-            isDisabled={false}
-            variation="primary"
-            children="Get started"
-          ></Button>
+          <Button size="large" variation="primary">Get started</Button>
         </Flex>
       </Flex>
       <Flex
@@ -148,20 +88,17 @@ export default function Hero() {
         justifyContent="center"
         alignItems="center"
         overflow="hidden"
-        shrink="0"
         alignSelf="stretch"
-        position="relative"
-        padding="0px 0px 0px 0px"
-        display="flex"
       >
         <Image
           width="720px"
+          height="50px"
           display="block"
           grow="1"
           shrink="1"
           basis="0"
           position="relative"
-          src="https://cdn.ampproject.org/ii/w720/s/www.ampproject.org/v/s/www.ampproject.org/amphtml/images/hero.png"
+          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZ1bGwlMjBzdGFjayUyMHdlYiUyMGFwcHxlbnwwfHx8fDE2OTY5NTQ3NzE&ixlib=rb-4.0.3&q=80&w=1080"
           alt="hero"
         ></Image>
       </Flex>
