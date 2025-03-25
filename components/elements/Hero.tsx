7d67b675622e151a460ca6b31193ad947640c2d9
tonchan1216 <cleartone1216@gmail.com>
2025-03-25 08:26:13 +0000

    image

--- components/elements/Hero.tsx
+++ components/elements/Hero.tsx
@@ -92,6 +92,7 @@ export default function Hero() {
           height={50}
           src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZ1bGwlMjBzdGFjayUyMHdlYiUyMGFwcHxlbnwwfHx8fDE2OTY5NTQ3NzE&ixlib=rb-4.0.3&q=80&w=1080"
           alt="hero"
+          className="hero-image"
         />
       </Flex>
     </Flex>
