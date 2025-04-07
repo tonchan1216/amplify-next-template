0dd4b434e12baccb2b3beba32be59cc1200f3e2f
tonchan1216 <cleartone1216@gmail.com>
2025-04-07 12:43:10 +0000

    priority

--- app/MainForm.tsx
+++ app/MainForm.tsx
@@ -6,48 +6,65 @@ import Form from 'next/form';
 import { Input, Flex, Button, Heading, Text, Badge, SwitchField } from '@aws-amplify/ui-react';
 import { postLinkForm } from "@/actions/postAction";
 
-function PriorityItem({ recommend }: { recommend: boolean }) {
-  return (
-    <Flex
-      gap="16px"
-      direction="row"
-      alignItems="flex-start"
-      padding="16px 16px 16px 16px"
-      backgroundColor="rgba(255,255,255,1)"
-    >
-      { recommend && <Badge size="small" variation="success">おすすめ</Badge>}
 
-      <Flex
-        gap="0"
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        grow="1"
-        shrink="1"
-        basis="0"
-      >
+const priorityMetadata: Record<string, { recommend: boolean; description: string }> = {
+  "駅チカ": {
+    recommend: true,
+    description: "駅から近い物件を優先",
+  },
+  "教育": {
+    recommend: false,
+    description: "教育環境を優先",
+  },
+  "コスパ": {
+    recommend: false,
+    description: "コストパフォーマンスを優先",
+  },
+  "耐震性": {
+    recommend: false,
+    description: "耐震性を優先",
+  },
+  "買い物": {
+    recommend: false,
+    description: "買い物環境を優先",
+  },
+  "閑静": {
+    recommend: false,
+    description: "閑静な環境を優先",
+  },
+  "新築": {
+    recommend: false,
+    description: "新築物件を優先",
+  },
+  "Option1": {
+    recommend: false,
+    description: "Option1を優先",
+  },
+  "Option2": {
+    recommend: false,
+    description: "Option2を優先",
+  },
+}
+
+function PriorityItem({ recommend, label, description, onToggle }: { recommend: boolean, label: string, description: string, onToggle: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <Flex direction="row" alignItems="flex-start" padding="0px 16px 8px 16px">
+      { recommend && <Badge size="small" variation="success" position="absolute">おすすめ</Badge>}
+
+      <Flex direction="row" justifyContent="space-between" alignItems="center" marginTop="30px">
         <Flex gap="0" direction="column">
-          <Text
-            fontFamily="Inter"
-            fontSize="16px"
-            fontWeight="700"
-            color="rgba(13,26,38,1)"
-            lineHeight="20px"
-          >T-Shirt</Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="16px"
-            fontWeight="400"
-            color="rgba(48,64,80,1)"
-            lineHeight="24px"
-            letterSpacing="0.01px"
-          >Classic Long Sleeve</Text>
+          <Text fontSize="16px" fontWeight="700" color="rgba(13,26,38,1)" lineHeight="20px">{label}</Text>
+          <Text fontSize="16px" fontWeight="400" color="rgba(48,64,80,1)" lineHeight="24px">{description}</Text>
         </Flex>
+
         <SwitchField
-          label="On"
-          defaultChecked={true}
+          label={label}
+          name={label}
+          defaultChecked={false}
           isDisabled={false}
           labelPosition="start"
+          onChange={onToggle}
+          isLabelHidden={true}
         ></SwitchField>
       </Flex>
   </Flex>
@@ -56,6 +73,18 @@ function PriorityItem({ recommend }: { recommend: boolean }) {
 }
 
 function Priority() {
+  const [priorities, setPriorities] = useState({
+    "駅チカ": false,
+    "教育": false,
+    "コスパ": false,
+    "耐震性": false,
+    "買い物": false,
+    "閑静": false,
+    "新築": false,
+    "Option1": false,
+    "Option2": false,
+  });
+
   return (
     <Flex direction="column">
       <Heading level={2}>Priority</Heading>
@@ -69,18 +98,17 @@ function Priority() {
         alignItems="center"
         alignSelf="stretch"
       >
-        <Flex direction="column">
-          <PriorityItem recommend={true}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
-        </Flex>
-
-        <Flex direction="column">
-          <PriorityItem recommend={false}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
-          <PriorityItem recommend={false}></PriorityItem>
+        <Flex direction="row" wrap={"wrap"} gap="16px" width="80%" alignItems="center" justifyContent="center">
+          {Object.keys(priorities).map((key) => (
+            <Flex key={key} direction="row" alignItems="center" gap="16px">
+              <PriorityItem
+                label={key}
+                recommend={priorityMetadata[key].recommend}
+                description={priorityMetadata[key].description}
+                onToggle={(e) => {setPriorities({...priorities, [e.target.name]: e.target.checked})}}
+              ></PriorityItem>
+            </Flex>
+          ))}
         </Flex>
       </Flex>
     </Flex>
