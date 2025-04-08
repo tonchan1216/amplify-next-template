9eda529067c36fe00708c826c5a5b373ef168137
tonchan1216 <cleartone1216@gmail.com>
2025-04-08 09:19:54 +0000

    route ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,9 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 
 // NextAuthの設定を行う
-export const authOptions = {
-  providers: [
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      if (token?.user) {
-        session.user.id = token.user.id;
-      }
-      return session;
-    },
-  },
-  session: {
-    // strategy: "jwt", // JWTセッションを使用
-  },
+const authOptions = {
+  providers: [],
+  secret: "3+p3gv+r+lFUnEXQ9hdf/83eXFx/gQ2lI03LJA0V/9k="
 };
 
 const handler = NextAuth(authOptions);
